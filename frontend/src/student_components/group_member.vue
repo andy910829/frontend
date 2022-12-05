f8fe98e0db7809159119a60d72f766cd29dcd4bb
andy910829 <71509005+andy910829@users.noreply.github.com>
2022-12-05 14:47:37 +0800

    Add files via upload

---
+++ frontend/src/student_components/group_member.vue
@@ -0,0 +1,153 @@
+<template>
+  <el-container>
+    <el-aside class="function-list">
+        <div class="title">小組資訊</div>
+        <el-scrollbar class="scrollbar">
+        <div class="display-groupinfo">小組ID:{{ group_info.group_id }}</div>
+        <div class="display-groupinfo">小組組長:{{ leaderName }}</div>
+        <li v-for="member in group_info.member" class="display-groupinfo">
+          小組組員:{{ member.student_id }}--{{ member.name }}
+        </li>
+      </el-scrollbar>
+    </el-aside>
+    <el-main class="main-display">
+      <timeline class="time-line" />
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import axios from "axios";
+import timeline from "../student_components/timeline.vue";
+
+export default {
+  data() {
+    return {
+      group_info: {},
+      leaderName: "",
+    };
+  },
+  components: {
+    timeline,
+  },
+  methods: {
+    get_group_info() {
+      const path = import.meta.env.VITE_API + "group_info";
+      axios
+        .get(path, { params: { group_id: sessionStorage.getItem("group_id") } })
+        .then((response) => {
+          this.group_info = response.data.group_info;
+          this.leaderName = this.group_info.leader.name;
+        });
+    },
+  },
+  created() {
+    this.get_group_info();
+  },
+};
+</script>
+
+<style scoped>
+@media screen {
+    .scrollbar {
+    display: block;
+    position: relative;
+    top:10%;
+    height: 65%;
+  }
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+    height: 10%;
+    font-size: 35px;
+    font-weight: 800;
+    background: rgba(255, 255, 255, 1);
+  }
+
+  .main-display {
+    position: absolute;
+    top: 10%;
+    left: 20%;
+    width: 80%;
+    height: 90%;
+    background: rgb(255, 255, 255, 0.7);
+  }
+
+  .function-list {
+    position: absolute;
+    width: 20%;
+    height: 90%;
+    top: 10%;
+    background: rgba(255, 255, 255, 0.5);
+  }
+
+  .display-groupinfo {
+    display: flex;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+    margin-top: 15%;
+    font-size: large;
+    font-weight: 500;
+    height: 50px;
+    width: 100%;
+    background-color: aliceblue;
+  }
+}
+@media screen and (max-width: 480px) {
+  .scrollbar {
+    display: block;
+    position: relative;
+    top:20%;
+    height: 65%;
+  }
+  .title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    left: 10%;
+    width: 80%;
+    height: 15%;
+    font-size: 20px;
+    font-weight: 800;
+    background: rgba(255, 255, 255, 1);
+  }
+  .function-list {
+    position: absolute;
+    width: 100%;
+    height: 50%;
+    top: 7%;
+    background: rgba(255, 255, 255, 0.7);
+  }
+  .main-display {
+    position: absolute;
+    top: 57%;
+    left: 0%;
+    width: 100%;
+    height: 43%;
+    background: rgb(255, 255, 255, 0.7);
+  }
+  .display-groupinfo {
+    display: flex;
+    position: relative;
+    top: 15%;
+    left: 10%;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5%;
+    font-size: large;
+    font-weight: 500;
+    height: 50px;
+    width: 80%;
+    background-color: aliceblue;
+  }
+  .time-line {
+    position: absolute;
+    left: 30%;
+  }
+}
+</style>
