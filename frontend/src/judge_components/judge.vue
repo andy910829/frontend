a4066010dfa404d483e97a7df84fdfc286cc19a9
andy910829 <71509005+andy910829@users.noreply.github.com>
2023-01-06 16:32:49 +0800

    Add files via upload

---
+++ frontend/src/judge_components/judge.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>這是評審頁面</div>
+      <div>{{user_type}}</div>
+    </div>
+  </template>
+  
+  <script>
+
+  export default {}
+
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
